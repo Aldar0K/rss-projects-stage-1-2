238b78f9a09ea05382507454aecf8a4cdf9eac4b
Aldar <aldarkonov@mail.ru>
2022-04-22 20:00:12 +0300

    feat: add new function blockBody

--- shelter/js/index.js
+++ shelter/js/index.js
@@ -1,6 +1,11 @@
 // Score.
 console.log('Score: 100 / 100');
 
+// Функция для блокировки и разблокировки скролла на странице.
+function blockBody () {
+    document.body.classList.toggle('body_lock');
+}
+
 // Меню бургер.
 const iconMenu = document.querySelector('.menu__icon');
 const menuBody = document.querySelector('.menu__body');
@@ -9,10 +14,10 @@ const menuLinks = document.querySelectorAll('.menu__link');
 
 if (iconMenu) {
    function toogleMenu () {
-        document.body.classList.toggle('body_lock');
         menuDropout.classList.toggle('menu__dropout_active');
         iconMenu.classList.toggle('menu__icon_active');
         menuBody.classList.toggle('menu__body_active');
+        blockBody();
     }
 
 menuDropout.addEventListener('click', toogleMenu);
