0e7345f6293b9a800f9dd9aacdaf10e158c98109
Aldar <aldarkonov@mail.ru>
2022-05-05 18:48:36 +0300

    feat: install and set babel

--- virtual-keyboard/src/index.js
+++ virtual-keyboard/src/index.js
@@ -1,1 +1,23 @@
-console.log('Hello!');
+// Создаем корневой контейнер root и помещаем его в body.
+const root = document.createElement('div');
+root.classList.add('root');
+root.id = 'root';
+document.body.append(root);
+
+// Создаем заголовок и помещаем его в root.
+const heading = document.createElement('h1');
+heading.textContent = 'Virtual keyboard RS School';
+root.append(heading);
+
+// Cоздаём экземпляр класса Game без конструктора.
+class Game {
+    name = 'Violin Charades'
+}
+const myGame = new Game()
+
+// Cоздаем параграф.
+const p = document.createElement('p')
+p.textContent = `I like ${myGame.game}.`
+root.append(p);
+
+import example from './images/example.jpg'
--- virtual-keyboard/webpack.config.js
+++ virtual-keyboard/webpack.config.js
@@ -1,11 +1,42 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: {
         main: path.resolve(__dirname, './src/index.js'),
     },
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: '[name].bundle.js',
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            title: 'Virtual keyboard',
+            template: path.resolve(__dirname, './src/template.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    module: {
+        rules: [
+            // JavaScript.
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            // Изображения.
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            // Шрифты и SVG.
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            }
+        ],
     },
 }
