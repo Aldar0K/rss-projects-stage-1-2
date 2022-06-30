c8579df1902d9223ba83828294d7bcb8addb2465
Aldar <aldarkonov@mail.ru>
2022-06-30 20:37:03 +0300

    feat: download dependencies, get apiKey

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '1751169aae08467b81e4892121501fd2', // получите свой ключ https://newsapi.org/
         });
     }
 }
