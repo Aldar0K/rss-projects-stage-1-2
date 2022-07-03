5ac79281b37b492b46959f2088ff892338dcb146
Aldar <aldarkonov@mail.ru>
2022-07-03 16:08:42 +0300

    refactor: change url in appLoader.ts for build

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '1751169aae08467b81e4892121501fd2', // получите свой ключ https://newsapi.org/
         });
     }
