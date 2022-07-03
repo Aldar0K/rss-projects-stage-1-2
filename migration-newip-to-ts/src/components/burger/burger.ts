2465af001c0f57e6307f958fd9e950ec28c60640
Aldar <aldarkonov@mail.ru>
2022-07-03 15:58:20 +0300

    feat: add menu burger to the header, adapt sources list

---
+++ migration-newip-to-ts/src/components/burger/burger.ts
@@ -0,0 +1,17 @@
+import './burger.css';
+
+const iconMenu = document.querySelector('.menu__icon') as HTMLDivElement;
+const sources = document.querySelector('.sources') as HTMLDivElement;
+
+function toogleMenu () {
+    iconMenu.classList.toggle('menu__icon_active');
+    sources.classList.toggle('sources_disable');
+
+}
+
+iconMenu.addEventListener('click', toogleMenu);
+
+// Закрытие меню при изменении разрешения.
+window.addEventListener('resize', () => {
+    if (iconMenu.classList.contains("menu__icon_active")) toogleMenu();
+});
--- migration-newip-to-ts/src/index.ts
+++ migration-newip-to-ts/src/index.ts
@@ -1,5 +1,6 @@
 import App from './components/app/app';
 import './global.css';
+import './components/burger/burger';
 
 const app = new App();
 app.start();
