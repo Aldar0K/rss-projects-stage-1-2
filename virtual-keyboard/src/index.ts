718f8ac9c5be420404bf3f4f1d1ad99403091640
Aldar <aldarkonov@mail.ru>
2022-07-07 21:19:17 +0300

    feat: implement main structure of keyboard

--- virtual-keyboard/src/index.ts
+++ virtual-keyboard/src/index.ts
@@ -1,1 +1,7 @@
+import { Keyboard } from './keyboard';
+
 console.log("Hello World!");
+
+const keyboard = new Keyboard(document.body);
+
+(window as any).keyboard = keyboard;
---
+++ virtual-keyboard/src/langs/ru.ts
@@ -0,0 +1,6 @@
+export const boardRu = {
+    'Digit1': '1',
+    'Digit2': '2',
+}
+
+export default boardRu;
