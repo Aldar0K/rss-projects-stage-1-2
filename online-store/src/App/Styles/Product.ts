fb1aa1e4610d393dae8b1946c8d980666a7161a9
Aldar <aldarkonov@mail.ru>
2022-07-13 22:37:10 +0300

    feat: add Products.DB.ts file with products objects

---
+++ online-store/src/App/Styles/Product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+    id: number;
+    name: string;
+    image: string;
+    color: string;
+    brand: string;
+    cameras: number;
+    release: number;
+    price: number;
+    amount: number;
+    inCart: boolean;
+}
